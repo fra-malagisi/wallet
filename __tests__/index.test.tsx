bf8ddf78d8f33ca88a2e2d25ec4063d4c5142100
francesco.malagisi <francesco.malagisi@accenture.com>
2022-03-01 14:49:43 +0100

    fix: turn @typescript-eslint/no-var-requires property on eslint to off

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,19 @@
+// __tests__/index.test.jsx
+import "@testing-library/jest-dom";
+
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", {
+      name: "Home",
+    });
+
+    console.log(heading);
+
+    expect(heading).toBeInTheDocument();
+  });
+});
