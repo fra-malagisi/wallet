a0aeda8f64520ac9fe373151e72f586485ab26d9
francesco.malagisi <francesco.malagisi@accenture.com>
2022-03-11 12:49:54 +0100

    fix: delete auth-guard

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,4 @@
 export { default as Header } from "./header";
 export { default as Layout } from "./layout";
 export { default as Accordion } from "./accordion";
-export { default as AuthGuard } from "./auth-guard";
 export { default as Home } from "./home";
