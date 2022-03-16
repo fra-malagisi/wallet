21ed2b0480f7f25805a21e1b658a554f972789f0
francesco.malagisi <francesco.malagisi@accenture.com>
2022-03-16 13:18:06 +0100

    feat: add react-hook-form

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -2,6 +2,7 @@ import * as dfd from "danfojs-node";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(req.body);
   const series: dfd.Series = new dfd.Series([1, 3, 5, undefined, 6, 8]);
   series.print();
   series.describe().print();
