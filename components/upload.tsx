73cf87cd6b1920b4c96682fc2a9be1eb18ed7937
francesco.malagisi <francesco.malagisi@accenture.com>
2022-03-16 13:25:52 +0100

    fix: add form type to upload

--- components/upload.tsx
+++ components/upload.tsx
@@ -1,18 +1,15 @@
-import { Input } from "@nextui-org/react";
-import { FC } from "react";
+import { FormElement, Input } from "@nextui-org/react";
+import React, { FC, HTMLAttributes } from "react";
 
 type UploadProps = {
   label: string;
-};
+} & HTMLAttributes<FormElement>;
 
-const Upload: FC<UploadProps> = ({ label }) => {
+const Upload: FC<UploadProps> = ({ label, onChange }) => {
   return (
     <div className="flex justify-center">
       <div className="mb-3 w-96">
-        <label htmlFor="formFile" className="form-label inline-block mb-2 text-gray-700">
-          {label}
-        </label>
-        <Input underlined color="primary" type="file" id="formFile" />
+        <Input underlined type="file" id="formFile" label={label} onChange={onChange} />
       </div>
     </div>
   );
